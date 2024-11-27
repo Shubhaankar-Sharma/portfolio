5e67f03698a4e001b15455340f3fb076ea82f8a2
spongeboi <hi@spongeboi.com>
2024-11-26 17:35:37 -0800

    text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 const About: React.FC = () => {
     return (
         <div>
-            <p className="text-2xl">WIP... stuff about my inspiration... my love for music, sitcoms... some philosophy... things i found interesting... travel... pictures</p>
+            <p className="text-2xl">WIP... stuff about my inspiration... my love for music, sitcoms, stratocasters... some philosophy... things i found interesting... travel... pictures</p>
         </div>
     );
  };
